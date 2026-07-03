410f1d7c60ff4cf6f2e45104b4fd30c9df47b2cc
Your Name <you@example.com>
2026-07-03 19:13:33 +0300

    Реализация классов Category и Product

--- src/products.py
+++ src/products.py
@@ -1,7 +1,29 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
 
 
+class Category:
+    name: str
+    description: str
+    products: list
+    category_count: int = 0
+    product_count: int = 0
 
-
+    def __init__(self, name, description, products=None):
+        self.name = name
+        self.description = description
+        self.products = products if products is not None else []
+        Category.product_count += len(products) if products else 0
+        Category.category_count += 1
 
 
 if __name__ == "__main__":
@@ -24,9 +46,11 @@ if __name__ == "__main__":
     print(product3.price)
     print(product3.quantity)
 
-    category1 = Category("Смартфоны",
-                         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-                         [product1, product2, product3])
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
 
     print(category1.name == "Смартфоны")
     print(category1.description)
@@ -34,10 +58,12 @@ if __name__ == "__main__":
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category2 = Category("Телевизоры",
-                         "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-                         [product4])
+    product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category2 = Category(
+        "Телевизоры",
+        "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        [product4],
+    )
 
     print(category2.name)
     print(category2.description)
