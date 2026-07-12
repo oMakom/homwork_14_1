efb64c1e33f319e12fc4282e0f0744ec1043740a
Your Name <you@example.com>
2026-07-12 14:43:10 +0300

    Правка кода для проверки под задание

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.products import Category, Product
+from src.products import Category, CategoryProductIterator, Product
 from src.utils import create_from_json, read_json_file
 
 if __name__ == "__main__":
@@ -7,56 +7,31 @@ if __name__ == "__main__":
     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
+    print(str(product1))
+    print(str(product2))
+    print(str(product3))
+
     category1 = Category(
         "Смартфоны",
         "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
         [product1, product2, product3]
     )
 
+    print(str(category1))
+
     print(category1.products)
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    category1.add_product(product4)
-    print(category1.products)
-    print(category1.product_count)
 
-    new_product = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 180000.0,
-         "quantity": 5})
-    print(new_product.name)
-    print(new_product.description)
-    print(new_product.price)
-    print(new_product.quantity)
-
-    new_product_2 = Product.new_product(
-        {"name": "Samsung Galaxy S23 Ultra", "description": "256GB, Серый цвет, 200MP камера", "price": 150000.0,
-         "quantity": 1})
-    print(new_product_2.name)
-    print(new_product_2.description)
-    print(new_product_2.price)
-    print(new_product_2.quantity)
-    print("-----------------Тестовый тескт для добавления------------------\n")
-    category2 = Category(
-        "Смартфоны------------",
-        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-        [product3]
-    )
-    print(category2.product_count)
-    category2.add_product(new_product)
-    category2.add_product(new_product_2)
-    print(category2.products)
-    print(category2.product_count)
-    print("-----------------Тестовый тескт для цены------------------\n")
-    new_product.price = 800
-    print(new_product.price)
-
-    new_product.price = -100
-    print(new_product.price)
-    new_product.price = 0
-    print(new_product.price)
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
+    print(f"Это пример строки категории: {category1}")
+    print("-----------------Тестовый тест для проверки задания со звездочкой------------------\n")
+    for i in CategoryProductIterator(category1):
+        print(i)
 
     # код для проверки Utils
 
-    print("-----------------Тестовый тескт для файла------------------\n")
+    print("-----------------Тестовый тест для файла------------------\n")
 
     data_file = read_json_file("data/products.json")
 
