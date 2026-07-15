9eb2623cb2207ed4c4d3ca4c5d6ed4b634e0b16a
Your Name <you@example.com>
2026-07-15 20:35:35 +0300

    Добавление тестов на введеный функционал

---
+++ tests/test_product_types.py
@@ -0,0 +1,15 @@
+
+
+def test_smartphone_init(first_product_smartphone):
+    """Проверка корректности создания"""
+    assert first_product_smartphone.efficiency == 95.5
+    assert first_product_smartphone.model == "S23 Ultra"
+    assert first_product_smartphone.memory == 256
+    assert first_product_smartphone.color == "Серый"
+
+
+def test_grass_init(first_product_grass):
+    """Проверка корректности создания"""
+    assert first_product_grass.country == "Россия"
+    assert first_product_grass.germination_period == "7 дней"
+    assert first_product_grass.color == "Зеленый"
