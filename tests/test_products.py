02c9a7054f34225e0c702d0b48f181ca8beb67d1
Your Name <you@example.com>
2026-07-21 20:50:09 +0300

    Тесты middle_price

--- tests/test_products.py
+++ tests/test_products.py
@@ -162,9 +162,19 @@ def test_add_product_allows_duplicates_by_name():
     assert str(order) == "Заказ №104, количество продуктов: 16 шт."
 
 
-def product_invalid():
+def test_product_invalid():
     """Ошибка добавления товара с нулевым количеством"""
     with pytest.raises(ValueError) as e:
         product_test = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
 
-    assert str(e.info) == "Товар с нулевым количеством не может быть добавлен"
+
+def test_middle_price(first_category):
+    """Проверка корректности подсчета среднего ценника всех товаров"""
+    assert  Category.middle_price(first_category) == 21488.1
+
+
+def test_middle_price_ZeroDivisionError():
+    """Ошибка, если 0 продуктов в категории (средняя цена приравнивается нулю)"""
+    category_empty = Category("Пустая категория", "Категория без продуктов", [])
+
+    assert Category.middle_price(category_empty) == 0
